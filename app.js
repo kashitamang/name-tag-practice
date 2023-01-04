cf67f62fe78a0a86a7fdb1098ce19283bee43865
KASHI TAMANG <tamang.kashi@gmail.com>
2023-01-03 18:49:11 -0800

    removing uneeded code from app.js

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-/* Imports */
-
-/* Get DOM Elements */
-
 const nameInput = document.getElementById('name-input');
 const nameDisplay = document.getElementById('name');
 const updateBtn = document.getElementById('update-btn');
@@ -9,16 +5,8 @@ const updateBtn = document.getElementById('update-btn');
 // console.log('nameDisplay', nameDisplay);
 // console.log('nameInput', nameInput);
 
-/* State */
-
-/* Events */
-
 updateBtn.addEventListener('click', () => {
     // console.log('clicking update button');
     nameDisplay.textContent = nameInput.value;
     nameInput.value = '';
 });
-
-/* Display Functions */
-
-// (don't forget to call any display functions you want to run on page load!)
