8fed905fc1c77bc71ddcd8536495a6867fe3cde0
KASHI TAMANG <tamang.kashi@gmail.com>
2023-01-03 18:35:15 -0800

    validated console.log of nameInput and nameDisplay

--- app.js
+++ app.js
@@ -2,6 +2,14 @@
 
 /* Get DOM Elements */
 
+const nameInput = document.getElementById('name-input');
+const nameDisplay = document.getElementById('name');
+
+console.log('nameDisplay', nameDisplay);
+console.log('nameInput', nameInput);
+
+nameDisplay.textContent = 'Kermit';
+
 /* State */
 
 /* Events */
