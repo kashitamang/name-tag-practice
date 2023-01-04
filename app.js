dadde3c401b1103343bcf079ccac877dcc320d66
KASHI TAMANG <tamang.kashi@gmail.com>
2023-01-03 18:55:14 -0800

    removing typo

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const nameInput = document.getElementById('name-input');
 const nameDisplay = document.getElementById('name');
 const updateBtn = document.getElementById('update-btn');
 
-console.log('nameDisplay', nameDisplay);
-console.log('nameInput', nameInput);
+// console.log('nameDisplay', nameDisplay);
+// console.log('nameInput', nameInput);
 
 updateBtn.addEventListener('click', () => {
-    console.log('clicking update button');
+    // console.log('clicking update button');
     nameDisplay.textContent = nameInput.value;
     nameInput.value = '';
 });
