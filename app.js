e8535a2162cd372391ed168f637a34b19e1142f0
KASHI TAMANG <tamang.kashi@gmail.com>
2023-01-03 18:38:19 -0800

    adding event listener to submit button, validated in console

--- app.js
+++ app.js
@@ -4,15 +4,19 @@
 
 const nameInput = document.getElementById('name-input');
 const nameDisplay = document.getElementById('name');
+const updateBtn = document.getElementById('update-btn');
 
 console.log('nameDisplay', nameDisplay);
 console.log('nameInput', nameInput);
 
+/* State */
 nameDisplay.textContent = 'Kermit';
 
-/* State */
-
 /* Events */
+
+updateBtn.addEventListener('click', () => {
+    console.log('clicking update button');
+});
 
 /* Display Functions */
 
